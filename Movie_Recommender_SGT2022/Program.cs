bef1dcd44bdb57d587ee92f5d98915a504783274
Katrina <Katrina@DESKTOP-L1GUFVH>
2022-10-13 20:04:52 +0300

    Add project files.

---
+++ Movie_Recommender_SGT2022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie_Recommender_SGT2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
